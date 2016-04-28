59eb242e7fd70180849dffe1c90f73ebff1b0f84
justinmichaels006 <justin@couchbase.com>
2016-04-28 16:56:59 -0700

    Add java samples

---
+++ src/main/java/com/couchbase/spark/example/QueryClicks.java
@@ -0,0 +1,42 @@
+package com.couchbase.spark.example;
+
+import com.couchbase.client.java.query.N1qlQuery;
+import com.couchbase.spark.japi.CouchbaseSparkContext;
+import com.couchbase.spark.rdd.CouchbaseQueryRow;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SQLContext;
+
+import java.io.IOException;
+import java.util.List;
+
+public class QueryClicks {
+
+    public static void main(String[] args) throws IOException {
+
+        // Configure Spark
+        SparkConf conf = new SparkConf()
+                .setAppName("CouchbaseQuery")
+                .setMaster("local[*]")
+                .set("com.couchbase.bucket.default", "");
+
+
+        // Create Java context from configuration
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        // Create SQL context from Java context
+        SQLContext sqlContext = new SQLContext(sc);
+
+        // Create Couchbase context using static method
+        CouchbaseSparkContext couchbaseSparkContext = CouchbaseSparkContext.couchbaseContext(sc);
+
+        //String query = ("SELECT * FROM `travel-sample` WHERE type = 'airline' LIMIT 10");
+        String query = ("select * from default where META(default).id LIKE 'click%'");
+
+        List<CouchbaseQueryRow> results = couchbaseSparkContext
+                .couchbaseQuery(N1qlQuery.simple(query))
+                .collect();
+
+        System.out.println(results);
+    }
+}
---
+++ src/main/java/com/couchbase/spark/example/QuerySample.java
@@ -0,0 +1,44 @@
+package com.couchbase.spark.example;
+
+import com.couchbase.client.java.query.N1qlQuery;
+import com.couchbase.spark.japi.CouchbaseSparkContext;
+import com.couchbase.spark.rdd.CouchbaseQueryRow;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SQLContext;
+
+import java.io.IOException;
+import java.util.List;
+
+public class QuerySample {
+
+    public static void main(String[] args) throws IOException {
+
+        // Configure Spark
+        SparkConf conf = new SparkConf()
+                .setAppName("CouchbaseQuery")
+                .setMaster("local[*]")
+                .set("com.couchbase.bucket.default", "");
+
+
+        // Create Java context from configuration
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        // Create SQL context from Java context
+        SQLContext sqlContext = new SQLContext(sc);
+
+        // Create Couchbase context using static method
+        CouchbaseSparkContext couchbaseSparkContext = CouchbaseSparkContext.couchbaseContext(sc);
+
+        //String query = ("SELECT * FROM `travel-sample` WHERE type = 'airline' LIMIT 10");
+        String query = ("select gender, count(*) from default group by gender");
+
+        List<CouchbaseQueryRow> results = couchbaseSparkContext
+                .couchbaseQuery(N1qlQuery.simple(query))
+                .collect();
+
+        System.out.println(results);
+
+
+    }
+}
