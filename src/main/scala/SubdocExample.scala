c201fdf8f43402de1bb6572f38fd754522229c25
justinmichaels006 <justin@couchbase.com>
2016-08-11 13:34:22 -0700

    Subdoc Example

--- src/main/scala/SubdocExample.scala
+++ src/main/scala/SubdocExample.scala
@@ -1,6 +1,6 @@
 import com.couchbase.client.java.document.JsonDocument
-import org.apache.spark.{SparkConf, SparkContext}
 import com.couchbase.spark._
+import org.apache.spark.{SparkConf, SparkContext}
 
 object SubdocSample {
 
@@ -13,29 +13,33 @@ object SubdocSample {
     val sc = new SparkContext(conf)
 
     val result = sc
-      .parallelize(Seq("airline_10"))
-      .couchbaseSubdocLookup(get = Seq("name", "iata"), exists = Seq("foobar"))
+      .couchbaseSubdocLookup(Seq("airline_10"), Seq("name", "iata"), exists = Seq("type"))
       .collect()
-
-    //val r2  = sc.couchbaseSubdocLookup(Seq("airline_10123"), Seq("name", "iata"))
-    val r3 = sc
-      .parallelize(Seq("airline_10123")) // Define Document IDs
-      .couchbaseGet[JsonDocument]() // Load them from Couchbase
-      .map(_.content()) // extract the content
-      .collect() // collect all data
-      .foreach(println) // print it out
 
     // Prints
     // SubdocLookupResult(
-    //    airline_10123,0,Map(name -> Texas Wings, iata -> TQ),Map(foobar -> false)
+    //    airline_10,0,Map(name -> 40-Mile Air, iata -> Q5)
     // )
     result.foreach(println)
 
     // Prints
-    // SubdocLookupResult(
-    //    airline_10123,0,Map(name -> Texas Wings, iata -> TQ),Map()
-    // )
-    //r2.foreach(println)
+    // airline_10 document
+    //{
+    //"id": 10,
+    //"type": "airline",
+    //"name": "40-Mile Air",
+    //"iata": "Q5",
+    //"icao": "MLA",
+    //"callsign": "MILE-AIR",
+    //"country": "United States"
+    //}
+
+    val r3 = sc
+      .parallelize(Seq("airline_10")) // Define Document IDs
+      .couchbaseGet[JsonDocument]() // Load document from Couchbase
+      .map(_.content()) // extract the content
+      .collect() // collect all data
+      .foreach(println) // print it out
 
   }
 }
